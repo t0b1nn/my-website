0d0bf230928ae62cfe8ed48b54524465f33cd616
Tobin Vadakkel <vadakkeltobin@gmail.com>
2026-02-13 19:52:42 +0000

    Finish clock

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function updateClock() {
     const time = new Date();
-    const hours = String(time.getHours()).padStart(2, "0")
-    const mins = String(time.getMinutes()).padStart(2, "0")
-    const secs = String(time.getSeconds()).padStart(2, "0")
-    document.getElementById("clock").textContent = `${hours}:${mins}:${secs}`
+    const hours = String(time.getHours()).padStart(2, "0");
+    const minutes = String(time.getMinutes()).padStart(2, "0");
+    const seconds = String(time.getSeconds()).padStart(2, "0");
+    document.getElementById("hour").textContent = hours;
+    document.getElementById("minute").textContent = minutes;
+    document.getElementById("second").textContent = seconds;
 }
 
 updateClock()
